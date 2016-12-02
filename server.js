0ce39319d2b81a734b6fc9617e3647aa3fecfd01
workwithfah <workwithfah@gmail.com>
2016-12-02 10:57:28 +0500

    Update server.js
    Update

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(methodOverride());
 // routes ======================================================================
 require('./app/routes.js')(app);
 
+
+
 // listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
