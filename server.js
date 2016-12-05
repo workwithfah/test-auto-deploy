239e1123648b97ac43ed15e65cb39a5b27a28bf8
workwithfah <workwithfah@gmail.com>
2016-12-05 10:30:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
 var port  	 = process.env.PORT || 3000; 				// set the port
-var database = require('./config/database'); 			// load the database config
+var database = require('./config/database'); 	
+	
+	// load the database config
 var morgan = require('morgan'); 		// log requests to the console (express4)
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
